3b6f199908a0077e62733c4d63dc0e6dc31f1add
lilys-corner <lillianyu0501@gmail.com>
2026-03-22 14:42:18 -0400

    We MOVE

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -1,4 +1,15 @@
 extends CharacterBody2D
 
+var direction: Vector2
+var speed := 50
+
+func _physics_process(_delta: float) -> void:
+	move()
+
+func move():
+	direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+	move_and_slide()
+
 func tool_use_emit():
 	print('tool')
